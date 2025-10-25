e38dfe7ff733f0fc801654223d5313aa97aec95d
Stephan <sggoosen3@gmail.com>
2025-10-25 11:21:39 +0200

    Replace MudBlazor UI with DevExtreme components

--- UI/Program.cs
+++ UI/Program.cs
@@ -1,9 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazor.Services;
 using UI;
 using UI.Services;
-using UI.Theming;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,11 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? builder.HostEnvironment.BaseAddress;
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
-builder.Services.AddMudServices(options =>
-{
-    options.SnackbarConfiguration.PreventDuplicates = true;
-    options.Theme = AppTheme.LightTheme;
-});
 builder.Services.AddScoped<AuthState>();
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<BookingApiClient>();
